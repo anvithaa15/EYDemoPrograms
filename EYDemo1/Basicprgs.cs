3b40918c129dcf690d43e79d74410f5e039b5fab
labuser <anvitha.a@gds.ey.com>
2023-02-16 10:29:10 +0000

    message

---
+++ EYDemo1/Basicprgs.cs
@@ -0,0 +1,72 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace EYDemo1
+//{
+//	class Basicprgs
+//	{
+//		static void Main(string[] args)
+//		{
+
+		
+			//const float pi = 3.142f;
+			//int r = int.Parse(Console.ReadLine());
+			//Console.WriteLine("area of circle is: " + (pi * r * r));
+			//Console.WriteLine("circumference of circle is: " + (2 * pi * r));
+			//Console.ReadLine();
+
+			//byte[] arr1 = { 2, 3, 4, 5 };
+			//Console.WriteLine("Array is: " + BitConverter.ToString(arr1));
+			//string s = Convert.ToBase64String(arr1);
+			//Console.WriteLine("Base 64 string is: {0}", s);
+			//Console.ReadLine();
+
+			
+			//	doApp();
+			//}
+
+			//static public void doApp()
+			//{
+
+			//	bool xbool = true;
+
+			//	byte xbyte = 250;
+
+			//sbyte xsbyte = -100;
+
+			//char xchar = 'a';
+
+			//decimal xdecimal = 87.9M;
+
+			//double xdouble = 651.09;
+
+			//float xfloat = 465.1f;
+
+			//int xint = -8909;
+
+			//uint xuint = 91881;
+
+			//long xlong = -1313131312;
+
+			//ulong xulong = 01919181;
+
+			//object xobject = new object();
+
+			//short xshort = -19;
+
+			//ushort xushort = 19;
+
+			//string xstring = "name";
+
+			//if (xbool == true && xbyte < 300)
+
+			//    Console.WriteLine("This expression evaluates to true");
+
+			//if (xshort > 6 || xint > -9000)
+			//    Console.WriteLine("This expression evaluates to false");
+//		}
+//	}
+//}
---
+++ EYDemo1/Program.cs
@@ -0,0 +1,54 @@
+﻿//Basic programs in c#
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EYDemo1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			//int a1 = 10;
+			//float a2 = 10.26543f;
+			//char ch = 'a';
+			//string s1 = "EY GDS";
+			//Console.WriteLine("Hello world");
+			//Console.WriteLine(a1);
+			//Console.WriteLine(a2);
+			//Console.Write(s1);
+			//Console.ReadKey();
+
+			Console.WriteLine("enter your name");
+			string name = Console.ReadLine();
+			Console.WriteLine("enter your roll num");
+			int num = int.Parse(Console.ReadLine());
+			Console.WriteLine("name: " + name);
+			Console.WriteLine("roll num: " + num);
+			Console.ReadLine();
+
+
+			Console.WriteLine("enter 1st number:");
+			int num1 = int.Parse(Console.ReadLine());
+			Console.WriteLine("enter 2nd number:");
+			int num2 = int.Parse(Console.ReadLine());
+			Console.WriteLine("Addition value is :" + (num1 + num2));
+			Console.WriteLine("Multiplication value is :" + (num1 * num2));
+			Console.WriteLine("Division value is :" + (num1 / num2));
+			Console.ReadLine();
+
+
+
+			int a = int.Parse(Console.ReadLine());
+			int i = int.Parse(Console.ReadLine());
+			int t = int.Parse(Console.ReadLine());
+			Console.WriteLine("SIMPLE INTREST :  " + (a * i * t / 100));
+			Console.ReadLine();
+
+		}
+	}
+}
+
